772b4f3a8714c764e4bfcca08d6068dcff1b5907
hanhnv <hanhnv@vng.com.vn>
2020-07-05 23:58:43 +0700

    Fix bug encode, decode

--- 19C11002_19C11003_19C11005/19C11002_19C11003_19C11005/source/ImgEncDec.cpp
+++ 19C11002_19C11003_19C11005/19C11002_19C11003_19C11005/source/ImgEncDec.cpp
@@ -49,23 +49,23 @@ int32_t ImgEncDec::encodeImg(const RawImg& rawImg, EncodedImg& encodedImg) {
 		for (uint8_t bitPos = 0; bitPos < 8; bitPos++) {
 			uint16_t intVal = 0;
 
-			modifyBit(intVal, 0, getBit(rawImg.buffer[rawBufferIdx], 8 - bitPos));
-			modifyBit(intVal, 1, getBit(rawImg.buffer[rawBufferIdx + 1], 8 - bitPos));
-			modifyBit(intVal, 2, getBit(rawImg.buffer[rawBufferIdx + 2], 8 - bitPos));
-			modifyBit(intVal, 3, getBit(rawImg.buffer[rawBufferIdx + 3], 8 - bitPos));
-			modifyBit(intVal, 4, getBit(rawImg.buffer[rawBufferIdx + 4], 8 - bitPos));
-			modifyBit(intVal, 5, getBit(rawImg.buffer[rawBufferIdx + 5], 8 - bitPos));
-			modifyBit(intVal, 6, getBit(rawImg.buffer[rawBufferIdx + 6], 8 - bitPos));
-			modifyBit(intVal, 7, getBit(rawImg.buffer[rawBufferIdx + 7], 8 - bitPos));
-			modifyBit(intVal, 8, getBit(rawImg.buffer[rawBufferIdx + 8], 8 - bitPos));
-			modifyBit(intVal, 9, getBit(rawImg.buffer[rawBufferIdx + 9], 8 - bitPos));
-			modifyBit(intVal, 10, getBit(rawImg.buffer[rawBufferIdx + 10], 8 - bitPos));
-			modifyBit(intVal, 11, getBit(rawImg.buffer[rawBufferIdx + 11], 8 - bitPos));
-			modifyBit(intVal, 12, getBit(rawImg.buffer[rawBufferIdx + 12], 8 - bitPos));
-			modifyBit(intVal, 13, getBit(rawImg.buffer[rawBufferIdx + 13], 8 - bitPos));
-			modifyBit(intVal, 14, getBit(rawImg.buffer[rawBufferIdx + 14], 8 - bitPos));
-			modifyBit(intVal, 15, getBit(rawImg.buffer[rawBufferIdx + 15], 8 - bitPos));
-
+			modifyBit(intVal, 0, getBit(rawImg.buffer[rawBufferIdx], 7 - bitPos));
+			modifyBit(intVal, 1, getBit(rawImg.buffer[rawBufferIdx + 1], 7 - bitPos));
+			modifyBit(intVal, 2, getBit(rawImg.buffer[rawBufferIdx + 2], 7 - bitPos));
+			modifyBit(intVal, 3, getBit(rawImg.buffer[rawBufferIdx + 3], 7 - bitPos));
+			modifyBit(intVal, 4, getBit(rawImg.buffer[rawBufferIdx + 4], 7 - bitPos));
+			modifyBit(intVal, 5, getBit(rawImg.buffer[rawBufferIdx + 5], 7 - bitPos));
+			modifyBit(intVal, 6, getBit(rawImg.buffer[rawBufferIdx + 6], 7 - bitPos));
+			modifyBit(intVal, 7, getBit(rawImg.buffer[rawBufferIdx + 7], 7 - bitPos));
+			modifyBit(intVal, 8, getBit(rawImg.buffer[rawBufferIdx + 8], 7 - bitPos));
+			modifyBit(intVal, 9, getBit(rawImg.buffer[rawBufferIdx + 9], 7 - bitPos));
+			modifyBit(intVal, 10, getBit(rawImg.buffer[rawBufferIdx + 10], 7 - bitPos));
+			modifyBit(intVal, 11, getBit(rawImg.buffer[rawBufferIdx + 11], 7 - bitPos));
+			modifyBit(intVal, 12, getBit(rawImg.buffer[rawBufferIdx + 12], 7 - bitPos));
+			modifyBit(intVal, 13, getBit(rawImg.buffer[rawBufferIdx + 13], 7 - bitPos));
+			modifyBit(intVal, 14, getBit(rawImg.buffer[rawBufferIdx + 14], 7 - bitPos));
+			modifyBit(intVal, 15, getBit(rawImg.buffer[rawBufferIdx + 15], 7 - bitPos));
+			
 			encodedImg.buffer[chunk] += intVal * powArr[bitPos];
 		}
 	}
@@ -97,28 +97,27 @@ int32_t ImgEncDec::decodeImg(const EncodedImg& encodedImg, RawImg& decodedImg) {
 
 	for (size_t chunk = 0; chunk < encodedImg.bufferLength; chunk++) {
 		float chunkVal = encodedImg.buffer[chunk];
-
 		size_t rawBufferIdx = chunk * 16;
 		for (uint8_t bitPos = 0; bitPos < 8; bitPos++) {
-			uint16_t intVal = chunkVal / powArr[bitPos];
+			uint16_t intVal = (uint16_t)(chunkVal / powArr[bitPos]);
 			chunkVal -= powArr[bitPos] * intVal;
-			
-			modifyBit(decodedImg.buffer[rawBufferIdx], 8 - bitPos, getBit(intVal, 0));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 1], 8 - bitPos, getBit(intVal, 1));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 2], 8 - bitPos, getBit(intVal, 2));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 3], 8 - bitPos, getBit(intVal, 3));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 4], 8 - bitPos, getBit(intVal, 4));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 5], 8 - bitPos, getBit(intVal, 5));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 6], 8 - bitPos, getBit(intVal, 6));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 7], 8 - bitPos, getBit(intVal, 7));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 8], 8 - bitPos, getBit(intVal, 8));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 9], 8 - bitPos, getBit(intVal, 9));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 10], 8 - bitPos, getBit(intVal, 10));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 11], 8 - bitPos, getBit(intVal, 11));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 12], 8 - bitPos, getBit(intVal, 12));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 13], 8 - bitPos, getBit(intVal, 13));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 14], 8 - bitPos, getBit(intVal, 14));
-			modifyBit(decodedImg.buffer[rawBufferIdx + 15], 8 - bitPos, getBit(intVal, 15));
+
+			modifyBit(decodedImg.buffer[rawBufferIdx], 7 - bitPos, getBit(intVal, 0));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 1], 7 - bitPos, getBit(intVal, 1));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 2], 7 - bitPos, getBit(intVal, 2));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 3], 7 - bitPos, getBit(intVal, 3));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 4], 7 - bitPos, getBit(intVal, 4));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 5], 7 - bitPos, getBit(intVal, 5));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 6], 7 - bitPos, getBit(intVal, 6));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 7], 7 - bitPos, getBit(intVal, 7));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 8], 7 - bitPos, getBit(intVal, 8));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 9], 7 - bitPos, getBit(intVal, 9));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 10], 7 - bitPos, getBit(intVal, 10));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 11], 7 - bitPos, getBit(intVal, 11));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 12], 7 - bitPos, getBit(intVal, 12));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 13], 7 - bitPos, getBit(intVal, 13));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 14], 7 - bitPos, getBit(intVal, 14));
+			modifyBit(decodedImg.buffer[rawBufferIdx + 15], 7 - bitPos, getBit(intVal, 15));
 		}
 	}
 
